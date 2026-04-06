251df1afb8dad3d3e26aff63208f4b8e584698e8
Eduardo <eduardoADS2008@gmail.com>
2026-04-06 12:52:00 -0300

    Adiciona arquivos At10 até At20

--- git-hub/At19.py
+++ git-hub/At19.py
@@ -1,4 +1,4 @@
-
+# Ler dois números inteiros e mostrar uma mensagem caso eles sejam iguais, caso contrário, mostrar outra mensagem.
 
 A = int(input("Digite um número inteiro: "))
 B = int(input("Digite um número inteiro: "))
