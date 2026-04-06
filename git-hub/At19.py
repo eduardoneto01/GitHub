ca8ba09cd18dd917795e9a43f92387c771d9135d
EduardoADSP1B <eduardofranciscofirmedasilva98@gmail.com>
2026-04-06 19:05:18 -0300

    Fix logic to compare two integers and show difference

--- git-hub/At19.py
+++ git-hub/At19.py
@@ -2,8 +2,8 @@
 
 A = int(input("Digite um número inteiro: "))
 B = int(input("Digite um número inteiro: "))
-if A == B:
-    print("Os números são iguais.")
+if A != B:
+    print("Os números são diferentes, a diferença entre eles é {A - B}.")
   
 else:
-    print("Os números são diferentes.")
+    print("Os números são iguais.")
